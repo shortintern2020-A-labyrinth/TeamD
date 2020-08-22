8cfa32fecd4bb6f4d0f31878124db3f9bb33641e
近藤大翔 <taisho90@icloud.com>
2020-08-22 11:03:50 +0900

    add swagger

--- django/company/views.py
+++ django/company/views.py
@@ -1,6 +1,3 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework import generics
 
 from .models import Question
