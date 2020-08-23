0353a7d186f26b0fe19f0b43ca900064c6a8788d
yoshikawataiki <yoshikawataiki@gmail.com>
2020-08-23 14:27:05 +0900

    fix naoki's(@Naoki206) commit

--- django/company/urls.py
+++ django/company/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import BookAPIView, VideoView
+from .util.models import sendMail
 
 urlpatterns = [
     path('', BookAPIView.as_view(), name='home'),
     path('video/', VideoView, name='home'),
+    path('sendmail', sendMail, name='mail')
 ]
