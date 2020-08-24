b5c28fc3addd7b50d50eb59f34b1059fe7b6a515
yoshikawataiki <yoshikawataiki@gmail.com>
2020-08-24 16:42:37 +0900

    update

--- django/company/views.py
+++ django/company/views.py
@@ -10,6 +10,7 @@ from .util.models import post_mail
 from django.utils import timezone
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 class CompanyAPIView(generics.ListAPIView):
     queryset = Company.objects.all()
@@ -48,26 +49,44 @@ def VideoPostValidation(data):
 #企業の仮登録
 @csrf_exempt
 def register_temporary_company(request):
-    company_name = request.POST['name']
-    email = request.POST['email']
-    password = request.POST['password']
-    description = request.POST['description']
-    is_accepted = 0 # 仮登録
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        description = data['description']
+        is_accepted = 0 # 仮登録
 
-     # 会員登録用トークン生成（メールアドレス + パスワード + システム日付のハッシュ値とする）
-    date = timezone.now()
-    tmp_str = email + password + date.strftime('%Y%m%d%H%M%S%f')
-    token = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
+        print(name)
 
-    #compnayテーブルにインサート
-    company = Company(name=company_name, email=email, password=password, description=description, is_accepted=is_accepted, token=token)
-    company.save()
+         # 会員登録用トークン生成（メールアドレス + パスワード + システム日付のハッシュ値とする）
+        date = timezone.now()
+        # tmp_str = email + password + date.strftime('%Y%m%d%H%M%S%f')
+        # token = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
+        token = "testtest"
+        #compnayテーブルにインサート
+        print 
+        company = Company(name=name, email=email, password=password, description=description, is_accepted=is_accepted, tokens=token)
+        company.save()
+
+        return JsonResponse(
+            {
+                'message': 'ok'
+            },
+            status=status.HTTP_200_OK
+        )
+    elif request.method == 'PUT':
+        print()
+    elif request.method == 'DELETE':
+        print()
 
     #運営に申請メール送信
-    subject="企業からの申請依頼のお知らせ"
-    to_email="A4sittyo@gmail.com"
-    body="企業名: " + company_name + "\n メールアドレス:" + email + "\n 企業概要: " + description + "\n　申請する rakutenpv.app/api//accept/company/?token=" + token
-    post_mail(subject, email, to_email, body)
+    # subject="企業からの申請依頼のお知らせ"
+    # to_email="A4sittyo@gmail.com"
+    # body="企業名: " + company_name + "\n メールアドレス:" + email + "\n 企業概要: " + description + "\n　申請する rakutenpv.app/api//accept/company/?token=" + token
+    # post_mail(subject, email, to_email, body)
 
 #仮登録企業の承認
 def accept_temporary_company(request):
