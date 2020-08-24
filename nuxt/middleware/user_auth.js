d3fa2e966fad4a761e2cbcc73f290e7f3501053f
近藤大翔 <taisho90@icloud.com>
2020-08-24 15:57:22 +0900

    omit un used code

--- nuxt/middleware/user_auth.js
+++ nuxt/middleware/user_auth.js
@@ -1,5 +1,4 @@
 export default function ({ store, redirect }) {
-  console.log('user_auth.jsが呼び出される')
   // // ログインしていない場合にログインページに飛ばす
   // if (!store.state.auth.loggedIn) {
   //   return redirect('/login')
