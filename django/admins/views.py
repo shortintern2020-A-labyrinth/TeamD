8001defb72d7bdca5277b77da7c9359f6894a2b6
Naoki206 <naoki020602060206@gmail.com>
2020-08-24 20:53:31 +0900

    post mail after accept company

--- django/admins/views.py
+++ django/admins/views.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from .util.models import post_mail
 
 from company.models import Company
 
@@ -17,11 +18,16 @@ def accept_company(request):
         try:
             # query_paramが指定されている場合の処理
             token = request.GET.get("token")
-            print(token)
             # 更新
             company = Company.objects.get(tokens=token)
             company.is_accepted = 1
             company.save()
+            # 企業に申請メール送信
+            email = company.email
+            subject = "申請承認のおしらせ"
+            from_email = "A4sittyo@gmail.com"
+            body = "申請が承認されました!\n"
+            post_mail(subject, from_email, [email], body)
             return Response(
                 {
                     'message': 'update success'
