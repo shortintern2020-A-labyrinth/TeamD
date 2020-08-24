ea20ae34d545a88be6b59b34fd4d7551b98c894a
Subaru-K <sbrkazuo@gmail.com>
2020-08-24 09:56:25 +0900

    youtubeURLを返すのは後回しにします

--- django/company/views.py
+++ django/company/views.py
@@ -46,7 +46,7 @@ def VideoView(request):
         keywords = "tag"
         privacyStatus = "public"
         '''
-        youtube_url = upload_youtube(file,title,description,category,keywords,privacyStatus)
+        upload_youtube(file,title,description,category,keywords,privacyStatus)
 
         res = {
             "message": "success!!!",
