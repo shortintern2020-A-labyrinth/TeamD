9b8a55e265cf2e902d141b2c948e75a01a41896f
Naoki206 <naoki020602060206@gmail.com>
2020-08-24 12:16:44 +0900

    company model modified

--- django/company/models.py
+++ django/company/models.py
@@ -42,7 +42,11 @@ class Company(models.Model):
             blank=False
     )
     email = models.EmailField()
-    password = models.CharField(max_length=50)
+    password = models.CharField(
+        max_length=140,
+        null=False,
+        blank=False
+    )
     description = models.TextField(
         null=False,
         blank=False
