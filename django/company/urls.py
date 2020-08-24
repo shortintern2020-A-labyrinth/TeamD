5de1126c822bb443021ad85925decb091088e923
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2020-08-24 17:49:06 +0900

    companyからadminsへ

--- django/company/urls.py
+++ django/company/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import register_temporary_company, accept_temporary_company
+from .views import register_temporary_company
 from .util.models import post_mail
 
 urlpatterns = [
     path('sendmail', post_mail, name='mail'),
     path('register', register_temporary_company),
-    path('accept/company', accept_temporary_company),
 ]
--- django/company/views.py
+++ django/company/views.py
@@ -12,6 +12,7 @@ import hashlib
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
+
 class CompanyAPIView(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
@@ -43,10 +44,14 @@ def video_view(request):
         print()
 
 # バリデーション
+
+
 def VideoPostValidation(data):
     return True
 
-#企業の仮登録
+# 企業の仮登録
+
+
 @csrf_exempt
 def register_temporary_company(request):
     if request.method == 'GET':
@@ -57,18 +62,19 @@ def register_temporary_company(request):
         email = data['email']
         password = data['password']
         description = data['description']
-        is_accepted = 0 # 仮登録
+        is_accepted = 0  # 仮登録
 
         print(name)
 
-         # 会員登録用トークン生成（メールアドレス + パスワード + システム日付のハッシュ値とする）
+        # 会員登録用トークン生成（メールアドレス + パスワード + システム日付のハッシュ値とする）
         date = timezone.now()
         # tmp_str = email + password + date.strftime('%Y%m%d%H%M%S%f')
         # token = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
         token = "testtest"
-        #compnayテーブルにインサート
-        print 
-        company = Company(name=name, email=email, password=password, description=description, is_accepted=is_accepted, tokens=token)
+        # compnayテーブルにインサート
+        print
+        company = Company(name=name, email=email, password=password,
+                          description=description, is_accepted=is_accepted, tokens=token)
         company.save()
 
         return JsonResponse(
@@ -82,26 +88,8 @@ def register_temporary_company(request):
     elif request.method == 'DELETE':
         print()
 
-    #運営に申請メール送信
+    # 運営に申請メール送信
     # subject="企業からの申請依頼のお知らせ"
     # to_email="A4sittyo@gmail.com"
     # body="企業名: " + company_name + "\n メールアドレス:" + email + "\n 企業概要: " + description + "\n　申請する rakutenpv.app/api//accept/company/?token=" + token
     # post_mail(subject, email, to_email, body)
-
-#仮登録企業の承認
-def accept_temporary_company(request):
-    if "token" in request.GET:
-        # query_paramが指定されている場合の処理
-        param_value = request.GET.get("token")
-        #更新
-        uniq_company = Company.objects.get(token=param_value)
-        uniq_company.is_accepted = 1
-        uniq_company.save() #UPDATE
-    else:
-        # query_paramが指定されていない場合の処理
-        return Response(
-            {
-                "message": "something wrong happened",
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
