335179adac1cc68b651294158179c6f5027c5096
近藤大翔 <taisho90@icloud.com>
2020-08-25 14:35:23 +0900

    change keywords inputs

--- nuxt/pages/company/postVideo.vue
+++ nuxt/pages/company/postVideo.vue
@@ -3,50 +3,26 @@
     <v-container>
       <v-row>
         <v-col cols="12">
-          <v-text-field
-            v-model="title"
-            label="動画タイトル※"
-            required
-          ></v-text-field>
+          <v-text-field v-model="title" label="動画タイトル※" required></v-text-field>
         </v-col>
 
         <v-col cols="12">
-          <v-textarea
-            v-model="description"
-            :counter="10"
-            label="動画詳細説明欄※"
-            required
-          ></v-textarea>
+          <v-textarea v-model="description" :counter="10" label="動画詳細説明欄※" required></v-textarea>
         </v-col>
 
         <v-col cols="12">
-          <v-select
-            v-model="selectedCategory"
-            :items="categories"
-            label="動画カテゴリ"
-            outlined
-          ></v-select>
+          <v-select v-model="selectedCategory" :items="categories" label="動画カテゴリ" outlined></v-select>
         </v-col>
 
         <!-- TODO: 本当はここは入力フォームを複数個作成するべきたけど、めんどくさくてできていない -->
         <v-col cols="12">
-          <v-text-field
-            v-model="keywords"
-            label="キーワード(半角スペース区切りで複数入力できます)"
-          ></v-text-field>
+          <v-text-field v-model="keywords" label="キーワード(カンマ区切りで複数入力できます)"></v-text-field>
         </v-col>
 
         <v-col cols="4">
-          <v-file-input
-            label="動画※"
-            filled
-            prepend-icon="mdi-movie"
-            @change="changeFile"
-          ></v-file-input>
+          <v-file-input label="動画※" filled prepend-icon="mdi-movie" @change="changeFile"></v-file-input>
         </v-col>
-        <v-col cols="8">
-          動画プレビュー画面
-        </v-col>
+        <v-col cols="8">動画プレビュー画面</v-col>
       </v-row>
 
       <v-card-actions>
@@ -62,12 +38,12 @@
 export default {
   data: () => ({
     valid: false,
-    title: '',
-    description: '',
+    title: 'testTITLE',
+    description: 'testDESC',
     categories: ['競馬', '伝統工芸'],
     selectedCategory: '',
     // カンマ区切りのkeywordが複数個入力されている
-    keywords: '',
+    keywords: '333 4444',
     companyID: 1,
     uploadFiles: [],
   }),
@@ -85,18 +61,14 @@ export default {
       formData.append('category_id', 1)
       // TODO: とりあえず固定値を入力している
       formData.append('company_id', 1)
-
-      // keywordsのset
-      const keywords = this.keywords.split(' ')
-      for (let i = 0; i < keywords.length; i++) {
-        const keyword = keywords[i]
-        formData.append('keywords[]', keyword)
-      }
+      // カンマ区切りにして一つの文字列にして送信する
+      const keyword = this.keywords.replace(' ', ',')
+      formData.append('keywords', keyword)
 
       // moviesのset
       for (let i = 0; i < this.uploadFiles.length; i++) {
         const movie = this.uploadFiles[i]
-        formData.append('movies[]', movie)
+        formData.append('movies', movie)
       }
 
       await this.$axios
