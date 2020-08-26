5b1b2570982f9e4005bc487c213d723d0df2c6aa
近藤大翔 <taisho90@icloud.com>
2020-08-27 08:26:52 +0900

    add comment

--- django/translate/gtrans.py
+++ django/translate/gtrans.py
@@ -1,5 +1,16 @@
 from googletrans import Translator
 
+""" 利用例
+    c = Translate()
+    text = '今日も頑張ります'
+    print(c.translate(text))
+
+    output: I'll do my best today too
+
+    現状こっちの方が問題がないのでこっち使う。
+    こっちだと利用回数に制限があるかもしれない
+"""
+
 class Translate():
     def __init__(self):
         self.client = Translator()
@@ -10,7 +21,3 @@ class Translate():
     def translate(self, text):
         res = self.client.translate(text, src=self.from_language, dest=self.target_language)
         return res.text
-
-c = Translate()
-text = '今日も頑張ります'
-print(c.translate(text))
